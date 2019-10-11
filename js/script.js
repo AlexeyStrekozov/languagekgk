c193b32e9673b3b402e2ca010b6df22dfbd1715a
unknown <alstrekozov@STD4.arteast.local>
2019-10-11 16:19:50 +0500

    блок с курсами
    Signed-off-by: unknown <alstrekozov@STD4.arteast.local>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let logo = document.querySelector('.header-content'),
-    header = document.querySelector('.header-wrap::after');
-    
+    header = document.querySelector('.header-wrap'),
+    buttonDetails = document.querySelectorAll('.courses-block-details-click'),
+    coursesPopupClose = document.querySelectorAll('.courses-block-popup-content-close'),
+    coursesPopup = document.querySelectorAll('.courses-block-popup'),
+    buttonDetailsArr = [];
 
 window.addEventListener('scroll', function() {
     let scrolled = window.scrollY;
     logo.style.transform = "translate(0px," + scrolled/3 +"%)";
-    header.style.transform = "translate(0px,-" + scrolled/100 +"%)";
+    header.style.transform = "translate(0px,-" + scrolled/80 +"%)";
 });
 
 
@@ -14,4 +17,16 @@ let forEach = function (array, callback) {
     callback.call(null, i, array[i]);
   }
 };
+
+for (var i = 0; i < buttonDetails.length; i++){
+    buttonDetailsArr.push(buttonDetails[i]);
+    buttonDetails[i].addEventListener('click', function(e){
+        let index = buttonDetailsArr.indexOf(e.target);
+        coursesPopup[index].classList.add('opnePopUP');
+        
+        coursesPopupClose[index].addEventListener('click', function(){
+            coursesPopup[index].classList.remove('opnePopUP');
+        });
+    });
+}
 
